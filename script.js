5f24f6b472de00c2862abfe0cf5c0419379d6e67
InsaneStudent1998 <106678831+1998ky262@users.noreply.github.com>
2022-12-08 22:25:09 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,44 @@
 let x;
 let y;
 // xはエンコードデコードする雑用変数です。
-// yにはtextdeta関数を実行することによって取得できるtextarea内の値が入っています。
+// yも雑用
 function textdata(){
   y=document.getElementById("area").value;
 return y
+  // 入力した文字を取得
 }
 function decode(){
 x=textdata();
 x=decodeURI(x);
+  // デコード
 copy();
 }
 function encode(){
 x=textdata();
 x=encodeURI(x);
+  // エンコード
 copy();
 }
 function copy(){
   textarea=document.getElementById("area").value;
-  y=document.getElementById("area");
+  // 変数textareaに入力した文字を入れます
 if (textarea==""){
 alert("文字が入力されていません");
 }else if(textarea=="1998はｸｿ!"){
+  //隠し機能
 x=fetch("ero.txt").then(response=>response.text())
   .then(code=>{
 navigator.clipboard.writeText(code);
 });
+  // ero.txtを取得してコピー
 alert("ん？ちょっと待てよ？、お前なんで秘密の隠し機能を知っている！");
 alert("エンコードしたわしの描いた超短編小説をクリップボードにコピーしといたぞ！");
 alert("もう二度とくるんじゃねぇよwwはははははww");
 }
 else{
 navigator.clipboard.writeText(x);
-document.getElementById("area").value="";//←ここ
+document.getElementById("area").value="";
+  //textarea内の文字を削除
 alert(x+"をコピーしました");
-
 }
 }
