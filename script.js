a485c8352db43a43e00faf5731383e98a991103f
haruymth <108157117+haruymth@users.noreply.github.com>
2022-12-18 14:03:53 +0900

    Update script.js

--- script.js
+++ script.js
@@ -22,28 +22,26 @@ copy();
 function copy(){
   textarea=document.getElementById("area").value;
   // 変数textareaに入力した文字を入れます
-if (textarea==""){
-alert("文字が入力されていません");
-}else if(textarea=="1998はｸｿ!"){
-  //隠し機能
-  let url = 'ero.txt'; //遅延情報のJSON
-  fetch(url)
-  .then(function (data) {
-    return data.json(); // 読み込むデータをJSONに設定
-    navigator.clipboard.writeText(data)
-  })
-    });
-  document.getElementById("area").value="";
-  //textarea内の文字を削除
-  // ero.txtを取得してコピー
-alert("ん？ちょっと待てよ？、お前なんで秘密の隠し機能を知っている！");
-alert("エンコードしたわしの描いた超短編小説をクリップボードにコピーしといたぞ！");
-alert("もう二度とくるんじゃねぇよwwはははははww");
-}
-else{
-navigator.clipboard.writeText(x);
-document.getElementById("area").value="";
-  //textarea内の文字を削除
-alert(x+"をコピーしました");
-}
+  if (textarea==""){
+    alert("文字が入力されていません");
+  }else if(textarea=="1998はｸｿ!"){
+    //隠し機能
+    let url = 'ero.txt'; //遅延情報のJSON
+    fetch(url)
+    .then(function (data) {
+      return data.json(); // 読み込むデータをJSONに設定
+      navigator.clipboard.writeText(data)
+    })
+    document.getElementById("area").value="";
+    //textarea内の文字を削除
+    // ero.txtを取得してコピー
+    alert("ん？ちょっと待てよ？、お前なんで秘密の隠し機能を知っている！");
+    alert("エンコードしたわしの描いた超短編小説をクリップボードにコピーしといたぞ！");
+    alert("もう二度とくるんじゃねぇよwwはははははww");
+  }else{
+    navigator.clipboard.writeText(x);
+    document.getElementById("area").value="";
+      //textarea内の文字を削除
+    alert(x+"をコピーしました");
+  }
 }
